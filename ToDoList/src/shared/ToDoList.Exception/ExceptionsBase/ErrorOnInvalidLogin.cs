2dce004eb6bc13a661496a150653cad2805b5972
oIthalo <ithalobarreto333@gmail.com>
2024-12-20 19:43:05 -0300

    feat: adding new exception message and invalid login exception

---
+++ ToDoList/src/shared/ToDoList.Exception/ExceptionsBase/ErrorOnInvalidLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.Exception.ExceptionsBase;
+
+public class ErrorOnInvalidLogin : ToDoListException
+{
+    public ErrorOnInvalidLogin() : base(MessagesException.LOGIN_INVALID)
+    {
+    }
+}

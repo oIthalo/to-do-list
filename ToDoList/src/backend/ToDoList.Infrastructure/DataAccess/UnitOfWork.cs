0125873b60f2d8ca8327b1cef09de4ab534dbb05
oIthalo <ithalobarreto333@gmail.com>
2024-12-20 14:18:02 -0300

    feat: commiting the repositories again

---
+++ ToDoList/src/backend/ToDoList.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using ToDoList.Domain.Repositories;
+
+namespace ToDoList.Infrastructure.DataAccess;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ToDoListDbContext _dbContext;
+
+    public UnitOfWork(ToDoListDbContext dbContext) =>
+        _dbContext = dbContext;
+
+    public async Task Commit() =>
+        await _dbContext.SaveChangesAsync();
+}

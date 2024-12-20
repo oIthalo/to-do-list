b2bfbbc54a7b249fce326eed8101b03c56164d94
oIthalo <ithalobarreto333@gmail.com>
2024-12-20 12:34:29 -0300

    feat: adding files of dependency injection

---
+++ ToDoList/src/backend/ToDoList.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Application.Services.Mapper;
+using ToDoList.Application.UseCases.User.Register;
+
+namespace ToDoList.Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddAutoMapper(services);
+        AddUseCases(services);
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        services.AddScoped(option => new AutoMapper.MapperConfiguration(autoMapperOptions =>
+        {
+            autoMapperOptions.AddProfile(new AutoMapping());
+        }).CreateMapper());
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+    }
+}

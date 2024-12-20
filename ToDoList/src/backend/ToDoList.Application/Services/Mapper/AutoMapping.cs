f2c0fff15b2665de4ec56ec37f08c94086cdf3f0
oIthalo <ithalobarreto333@gmail.com>
2024-12-20 19:45:52 -0300

    feat: adding login use case and improvement register use case

--- ToDoList/src/backend/ToDoList.Application/Services/Mapper/AutoMapping.cs
+++ ToDoList/src/backend/ToDoList.Application/Services/Mapper/AutoMapping.cs
@@ -14,5 +14,6 @@ public class AutoMapping : Profile
     private void RequestToDomain()
     {
         CreateMap<RegisterUserRequest, User>();
+        CreateMap<DoLoginRequest, User>();
     }
 }
---
+++ ToDoList/src/backend/ToDoList.Application/UseCases/Login/DoLogin/ILoginUseCase.cs
@@ -0,0 +1,9 @@
+﻿using ToDoList.Communication.Requests;
+using ToDoList.Communication.Responses;
+
+namespace ToDoList.Application.UseCases.Login.DoLogin;
+
+public interface ILoginUseCase
+{
+    Task<RegisterUserResponse> Execute(DoLoginRequest request);
+}

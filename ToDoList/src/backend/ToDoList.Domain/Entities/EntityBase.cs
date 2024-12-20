edf51e5f211806b8a6bf2d47783b59d2162368db
oIthalo <ithalobarreto333@gmail.com>
2024-12-20 12:29:46 -0300

    feat: creating user entity

---
+++ ToDoList/src/backend/ToDoList.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.Domain.Entities;
+
+public abstract class EntityBase
+{
+    public long Id { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}

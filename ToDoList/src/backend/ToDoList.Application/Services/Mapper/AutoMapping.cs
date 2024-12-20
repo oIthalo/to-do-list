1056aafda219942d0dbc6439482fff6e87dea15b
oIthalo <ithalobarreto333@gmail.com>
2024-12-20 12:33:08 -0300

    feat: adding automapper

---
+++ ToDoList/src/backend/ToDoList.Application/Services/Mapper/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ToDoList.Communication.Requests;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Application.Services.Mapper;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToDomain();
+    }
+
+    private void RequestToDomain()
+    {
+        CreateMap<RegisterUserRequest, User>();
+    }
+}

c99c3dd64e29531a5fee7160eda79110ca91b91d
oIthalo <ithalobarreto333@gmail.com>
2024-12-21 11:28:36 -0300

    feat: creating database with API

---
+++ ToDoList/src/backend/ToDoList.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ToDoList.Infrastructure.Extensions;
+
+public static class ConfigurationExtension
+{
+    public static string ConnectionStringBuilder(this IConfiguration configuration) => configuration.GetConnectionString("Connection")!;
+}

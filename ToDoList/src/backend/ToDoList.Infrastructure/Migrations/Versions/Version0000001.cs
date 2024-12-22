ff38b7c2c83d44b8399c967d5595beeaf71aff4c
oIthalo <ithalobarreto333@gmail.com>
2024-12-21 22:14:39 -0300

    feat: creating database with API

---
+++ ToDoList/src/backend/ToDoList.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace ToDoList.Infrastructure.Migrations.Versions;
+
+[Migration(DataBaseVersions.USER_TABLE, "The first version of database")]
+public class Version0000001 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Users")
+            .WithColumn("Name").AsString(50).NotNullable()
+            .WithColumn("Email").AsString(160).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable()
+            .WithColumn("Identifier").AsGuid().NotNullable();
+    }
+}

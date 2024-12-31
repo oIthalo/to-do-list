13f9181f9f4870d4220ef827ec3a97fbfcec1e9c
oIthalo <ithalobarreto333@gmail.com>
2024-12-31 13:08:28 -0300

    feat: configuring swagger to get the token on request

--- ToDoList/src/backend/ToDoList.API/Program.cs
+++ ToDoList/src/backend/ToDoList.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using ToDoList.API.Filters;
 using ToDoList.API.Token;
 using ToDoList.Application;
@@ -13,7 +15,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    var scheme = "Bearer";
+
+    opts.AddSecurityDefinition(scheme, new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = scheme
+    });
+
+    opts.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = scheme
+                },
+                Scheme = "oauth2",
+                Name = scheme,
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 

cca13a7f4011c7d5837969d4ed94977864925749
oIthalo <ithalobarreto333@gmail.com>
2025-01-02 17:44:28 -0300

    feat: adding table and entity task

---
+++ ToDoList/src/backend/ToDoList.Domain/Enums/EStatusTask.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoList.Domain.Enums;
+
+public enum EStatusTask
+{
+    Todo = 0,
+    Done = 1,
+}
---
+++ ToDoList/src/backend/ToDoList.Infrastructure/Migrations/Versions/Version0000002.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace ToDoList.Infrastructure.Migrations.Versions;
+
+[Migration(DataBaseVersions.TASKS_TABLE, "Adding table to get tasks")]
+public class Version0000002 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Tasks")
+            .WithColumn("Title").AsString(80).NotNullable()
+            .WithColumn("Description").AsString(500).NotNullable()
+            .WithColumn("Status").AsInt16().NotNullable();
+    }
+}

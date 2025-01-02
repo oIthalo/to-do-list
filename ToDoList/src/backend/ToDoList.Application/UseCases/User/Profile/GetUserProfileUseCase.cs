5818cb7a978de9c52f6871542a15bcd0835430ed
oIthalo <ithalobarreto333@gmail.com>
2025-01-02 15:10:43 -0300

    feat: getting user profile

---
+++ ToDoList/src/backend/ToDoList.Application/UseCases/User/Profile/GetUserProfileUseCase.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ToDoList.Communication.Responses;
+using ToDoList.Domain.Services.LoggedUser;
+
+namespace ToDoList.Application.UseCases.User.Profile;
+
+public class GetUserProfileUseCase : IGetUserProfileUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IMapper _mapper;
+
+    public GetUserProfileUseCase(
+        ILoggedUser loggedUser, 
+        IMapper mapper)
+    {
+        _loggedUser = loggedUser;
+        _mapper = mapper;
+    }
+
+    public async Task<UserProfileResponse> Execute()
+    {
+        var user = await _loggedUser.User();
+        return _mapper.Map<UserProfileResponse>(user);
+    }
+}
---
+++ ToDoList/src/backend/ToDoList.Application/UseCases/User/Profile/IGetUserProfileUseCase.cs
@@ -0,0 +1,8 @@
+﻿using ToDoList.Communication.Responses;
+
+namespace ToDoList.Application.UseCases.User.Profile;
+
+public interface IGetUserProfileUseCase
+{
+    Task<UserProfileResponse> Execute();
+}

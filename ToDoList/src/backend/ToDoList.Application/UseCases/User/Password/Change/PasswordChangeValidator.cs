c9c6017c1b956ae6c6355b84e6fe8829430a89bd
oIthalo <ithalobarreto333@gmail.com>
2025-01-07 04:06:13 -0300

    test: change password use case tests

--- ToDoList/src/backend/ToDoList.Application/UseCases/User/Password/Change/PasswordChangeValidator.cs
+++ ToDoList/src/backend/ToDoList.Application/UseCases/User/Password/Change/PasswordChangeValidator.cs
@@ -10,10 +10,12 @@ public class PasswordChangeValidator : AbstractValidator<PasswordChangeRequest>
     {
         RuleFor(x => x.Password).NotEmpty().WithMessage(MessagesException.INVALID_PASSWORD);
         RuleFor(x => x.NewPassword).NotEmpty().WithMessage(MessagesException.PASSWORD_EMPTY);
+
         When(x => !string.IsNullOrWhiteSpace(x.Password), () =>
         {
             RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(8).WithMessage(MessagesException.INVALID_PASSWORD);
         });
+
         When(x => !string.IsNullOrWhiteSpace(x.NewPassword), () =>
         {
             RuleFor(x => x.NewPassword.Length).GreaterThanOrEqualTo(8).WithMessage(MessagesException.SHORT_PASSWORD);

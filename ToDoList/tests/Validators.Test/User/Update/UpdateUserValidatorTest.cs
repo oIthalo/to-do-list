51e66b17695cccf938434f39d1ebda99896031fd
oIthalo <ithalobarreto333@gmail.com>
2025-01-07 00:08:23 -0300

    fix: resolving error on tests architecture

---
+++ ToDoList/tests/Validators.Test/User/Update/UpdateUserValidatorTest.cs
@@ -0,0 +1,55 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using ToDoList.Application.UseCases.User.Update;
+using ToDoList.Exception;
+
+namespace Validators.Test.User.Update;
+
+public class UpdateUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var request = UpdateUserRequestBuilder.Build();
+        var result = new UpdateUserValidator().Validate(request);
+
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var request = UpdateUserRequestBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = new UpdateUserValidator().Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(x => x.ErrorMessage.Equals(MessagesException.USER_NAME_EMPTY));
+    }
+
+    [Fact]
+    public void Error_Email_Empty()
+    {
+        var request = UpdateUserRequestBuilder.Build();
+        request.Email = string.Empty;
+
+        var result = new UpdateUserValidator().Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(x => x.ErrorMessage.Equals(MessagesException.EMAIL_EMPTY));
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        var request = UpdateUserRequestBuilder.Build();
+        request.Email = "invalidemail.com";
+
+        var result = new UpdateUserValidator().Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(x => x.ErrorMessage.Equals(MessagesException.EMAIL_INVALID));
+    }
+}

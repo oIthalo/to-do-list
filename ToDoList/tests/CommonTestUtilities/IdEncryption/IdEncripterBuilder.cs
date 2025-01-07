e702c96ef75a291c3255a0cb158434674cf15886
oIthalo <ithalobarreto333@gmail.com>
2025-01-07 00:37:06 -0300

    fix: resolving project issues

---
+++ ToDoList/tests/CommonTestUtilities/IdEncryption/IdEncripterBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Sqids;
+
+namespace CommonTestUtilities.IdEncryption;
+
+public class IdEncripterBuilder
+{
+    public static SqidsEncoder<long> Build()
+    {
+        return new SqidsEncoder<long>(new()
+        {
+            MinLength = 3,
+            Alphabet = "LU0KYyMDrN8fS64VFmkOoaZGXqhcPEu7Ijleip2vnTtQz5JwRsHdAgWCbx391B"
+        });
+    }
+}
---
+++ ToDoList/tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,11 @@
+﻿using ToDoList.Application.UseCases.User.Register;
+
+namespace UseCases.Test.User.Register;
+
+public class RegisterUserUseCaseTest
+{
+    private static RegisterUserUseCase CreateUseCase()
+    {
+        throw new NotImplementedException();
+    }
+}

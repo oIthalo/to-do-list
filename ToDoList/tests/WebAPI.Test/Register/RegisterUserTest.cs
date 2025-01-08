9a0c33e32d29ff4db385924747e15e965ba1bc2e
oIthalo <ithalobarreto333@gmail.com>
2025-01-08 07:01:53 -0300

    test: register user integration test

---
+++ ToDoList/tests/WebAPI.Test/Register/RegisterUserTest.cs
@@ -0,0 +1,48 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Net;
+using System.Text.Json;
+using ToDoList.Exception;
+
+namespace WebAPI.Test.Register;
+
+public class RegisterUserTest : ToDoListClassFixture
+{
+    private const string METHOD = "user/register";
+
+    public RegisterUserTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = RegisterUserRequestBuilder.Build();
+        var response = await DoPost(METHOD, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(request.Name);
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = RegisterUserRequestBuilder.Build();
+        request.Name = string.Empty;
+
+        var response = await DoPost(METHOD, request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var error = responseData.RootElement.GetProperty("errorMessages")[0].GetString();
+
+        error.Should().Be(MessagesException.USER_NAME_EMPTY);
+    }
+}
--- ToDoList/tests/WebAPI.Test/ToDoListClassFixture.cs
+++ ToDoList/tests/WebAPI.Test/ToDoListClassFixture.cs
@@ -1,6 +1,16 @@
-﻿namespace WebAPI.Test;
+﻿using Azure.Core;
+using System.Net.Http.Json;
 
-public class ToDoListClassFixture : CustomWebApplicationFactory
+namespace WebAPI.Test;
+
+public class ToDoListClassFixture : IClassFixture<CustomWebApplicationFactory>
 {
+    private readonly HttpClient _httpClient;
 
+    public ToDoListClassFixture(CustomWebApplicationFactory factory) => _httpClient = factory.CreateClient();
+
+    protected async Task<HttpResponseMessage> DoPost(string method, object request)
+    {
+        return await _httpClient.PostAsJsonAsync(method, request);
+    }
 }

77a2179c2a3f640c2e694ca27520792f33a88def
oIthalo <ithalobarreto333@gmail.com>
2025-01-11 05:58:01 -0300

    feat: adding migration to delete refresh tokens and tasks on delete user

--- ToDoList/src/backend/ToDoList.Infrastructure/Migrations/DataBaseVersions.cs
+++ ToDoList/src/backend/ToDoList.Infrastructure/Migrations/DataBaseVersions.cs
@@ -5,4 +5,5 @@ public abstract class DataBaseVersions
     public const int USER_TABLE = 1;
     public const int TASKS_TABLE = 2;
     public const int REFRESH_TOKENS_TABLE = 3;
+    public const int CASCADE_DELETION = 4;
 }
---
+++ ToDoList/src/backend/ToDoList.Infrastructure/Migrations/Versions/Version0000004.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace ToDoList.Infrastructure.Migrations.Versions;
+
+[Migration(DataBaseVersions.CASCADE_DELETION, "Cascade deletion on tasks and refresh tokens from users")]
+public class Version0000004 : VersionBase
+{
+    public override void Up()
+    {
+        Delete.ForeignKey("FK_Users_Tasks_Identifier").OnTable("Tasks");
+        Create.ForeignKey("FK_Users_Tasks_Identifier")
+            .FromTable("Tasks").ForeignColumn("UserIdentifier")
+            .ToTable("Users").PrimaryColumn("Identifier")
+            .OnDelete(System.Data.Rule.Cascade);
+
+        Delete.ForeignKey("FK_RefreshTokens_User_Id").OnTable("RefreshTokens");
+        Create.ForeignKey("FK_RefreshTokens_User_Id")
+            .FromTable("RefreshTokens").ForeignColumn("UserId")
+            .ToTable("Users").PrimaryColumn("Id")
+            .OnDelete(System.Data.Rule.Cascade);
+    }
+}
